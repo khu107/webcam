1bbee4d9411f0b3d3ac7fe116f23cbf97b0da6dd
metablog <khu10735@naver.com>
2023-04-05 01:41:10 +0900

    setting

---
+++ src/WebcamImage.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Webcam from 'react-webcam';
+import { useCallback, useRef, useState } from 'react';
+export default function WebcamImage() {
+  const [img, setImg] = useState(null);
+  const webcamRef = useRef(null);
+
+  const videoConstraints = {
+    width: 420,
+    height: 420,
+    facingMode: 'user',
+  };
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImg(imageSrc);
+  }, [webcamRef]);
+  return (
+    <div className="">
+      {img === null ? (
+        <>
+          <Webcam
+            audio={false}
+            mirrored={true}
+            height={200}
+            width={200}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            videoConstraints={videoConstraints}
+          />
+          <button onClick={capture}>Capture photo</button>
+        </>
+      ) : (
+        <>
+          <img src={img} alt="screenshot" />
+          <button onClick={() => setImg(null)}>Retake</button>
+          <h1 className="text-xl font-bold underline">Hello world!</h1>
+        </>
+      )}
+    </div>
+  );
+}
